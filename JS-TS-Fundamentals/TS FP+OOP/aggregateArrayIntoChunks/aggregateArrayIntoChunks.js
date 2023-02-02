53a1696dce1ae23a82f12326a4750fd698aabd01
Saracier <105667230+Saracier@users.noreply.github.com>
2023-02-02 15:17:37 +0100

    changes 02.02.2023

--- JS-TS-Fundamentals/TS FP+OOP/aggregateArrayIntoChunks/aggregateArrayIntoChunks.js
+++ JS-TS-Fundamentals/TS FP+OOP/aggregateArrayIntoChunks/aggregateArrayIntoChunks.js
@@ -1,5 +1,64 @@
 // Stwórz funkcję aggregateIntoChunks agregującą wszystkie elementy array na losowej długości chunki
 // Każdy chunk powienien mieć od 4 do 7 elementów, ostatni chunk też powinien być długości od 4 do 7
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var aggregateIntoChunks2 = function (array) {
+    var finalArray = [];
+    var copiedArray = __spreadArray([], array, true);
+    var helperArray = [];
+    if (array.length <= 7) {
+        return array;
+    }
+    main: for (var i = 0; i < array.length; i++) {
+        var randomForThisIteration = generateRandom();
+        helperArray.push(array[i]);
+        if ((helperArray.length >= randomForThisIteration && Math.random() < 0.3) ||
+            helperArray.length == 7) {
+            finalArray.push(helperArray);
+            helperArray = [];
+        }
+        if (i + 8 === array.length) {
+            if (helperArray.length === 0) {
+                for (var n = 0; n + i < array.length - 4; n++) {
+                    helperArray.push(array[n + i]);
+                }
+                finalArray.push(helperArray);
+                helperArray = [];
+                for (var n = 0; n + i + 4 < array.length; n++) {
+                    helperArray.push(array[n + i]);
+                }
+                finalArray.push(helperArray);
+                helperArray = [];
+                break main;
+            }
+        }
+        if (array.length - 4 === i && helperArray.length < 2) {
+            for (var n = 1; n + i < array.length; n++) {
+                helperArray.push(array[n + i]);
+            }
+            finalArray.push(helperArray);
+            helperArray = [];
+            break main;
+        }
+        if (array.length === i) {
+            finalArray.push(helperArray);
+            helperArray = [];
+        }
+    }
+    for (var i = 0; i < finalArray.length; i++) {
+        if (finalArray[i].length < 4) {
+            console.log('wyszła lipa');
+        }
+    }
+    return finalArray;
+};
 var aggregateIntoChunks = function (array) {
     var finalArray = [];
     var helperArray = [];
@@ -7,7 +66,7 @@ var aggregateIntoChunks = function (array) {
         var randomForThisIteration = generateRandom();
         helperArray.push(array[i]);
         if ((helperArray.length >= randomForThisIteration && Math.random() < 0.3) ||
-            helperArray.length == 7 || array[i + 1] == 'undefined') {
+            helperArray.length == 7) {
             finalArray.push(helperArray);
             helperArray = [];
         }
@@ -27,6 +86,6 @@ function generateRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 // Sprawdzenie działania funckji:
-var alphabet = 'abcdl'.split('');
-var chunks = aggregateIntoChunks(alphabet);
+var alphabet = '12345678'.split('');
+var chunks = aggregateIntoChunks2(alphabet);
 console.log(chunks);
--- JS-TS-Fundamentals/TS FP+OOP/aggregateArrayIntoChunks/aggregateArrayIntoChunks.ts
+++ JS-TS-Fundamentals/TS FP+OOP/aggregateArrayIntoChunks/aggregateArrayIntoChunks.ts
@@ -1,6 +1,63 @@
 // Stwórz funkcję aggregateIntoChunks agregującą wszystkie elementy array na losowej długości chunki
 // Każdy chunk powienien mieć od 4 do 7 elementów, ostatni chunk też powinien być długości od 4 do 7
 
+// aggregateIntoChunks2 nie działa. Warunki na awaryjne przerwanie są skopane. korzystać ze zwyklego aggregateIntoChunks
+const aggregateIntoChunks2 = (array: string[]) => {
+  let finalArray: string[][] = [];
+  let copiedArray = [...array];
+  let helperArray: string[] = [];
+  if (array.length <= 7) {
+    return array;
+  }
+
+  main: for (let i = 0; i < array.length; i++) {
+    let randomForThisIteration = generateRandom();
+    helperArray.push(array[i]);
+    if (
+      (helperArray.length >= randomForThisIteration && Math.random() < 0.3) ||
+      helperArray.length == 7
+    ) {
+      finalArray.push(helperArray);
+      helperArray = [];
+    }
+    if (i + 8 === array.length) {
+      if (helperArray.length === 0) {
+        for (let n = 0; n + i < array.length - 4; n++) {
+          helperArray.push(array[n + i]);
+        }
+        finalArray.push(helperArray);
+        helperArray = [];
+        for (let n = 0; n + i + 4 < array.length; n++) {
+          helperArray.push(array[n + i]);
+        }
+        finalArray.push(helperArray);
+        helperArray = [];
+        break main;
+      }
+    }
+    if (array.length - 4 === i && helperArray.length < 2) {
+      for (let n = 0; n + i < array.length; n++) {
+        helperArray.push(array[n + i]);
+      }
+      finalArray.push(helperArray);
+      helperArray = [];
+      break main;
+    }
+    if (array.length === i) {
+      finalArray.push(helperArray);
+      helperArray = [];
+    }
+  }
+
+  for (let i = 0; i < finalArray.length; i++) {
+    if (finalArray[i].length < 4) {
+      console.log('wyszła lipa');
+    }
+  }
+
+  return finalArray;
+};
+
 const aggregateIntoChunks = (array: string[]) => {
   let finalArray: string[][] = [];
   let helperArray: string[] = [];
@@ -9,8 +66,7 @@ const aggregateIntoChunks = (array: string[]) => {
     helperArray.push(array[i]);
     if (
       (helperArray.length >= randomForThisIteration && Math.random() < 0.3) ||
-      helperArray.length == 7 ||
-      array[i + 1] == 'undefined'
+      helperArray.length == 7
     ) {
       finalArray.push(helperArray);
       helperArray = [];
@@ -32,6 +88,6 @@ function generateRandom(min = 4, max = 7) {
 }
 
 // Sprawdzenie działania funckji:
-const alphabet = 'abcdl'.split('');
-const chunks = aggregateIntoChunks(alphabet);
+const alphabet = 'abl'.split('');
+const chunks = aggregateIntoChunks2(alphabet);
 console.log(chunks);
