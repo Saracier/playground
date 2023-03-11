e88d21c17a3a48ab7969a66671f2453f48f7772a
Saracier <105667230+Saracier@users.noreply.github.com>
2023-03-11 14:09:01 +0100

    finished dvdLogoAnimation

--- JS-TS-Fundamentals/TS in HTML/dvdLogoAnimation/dvdLogoAnimation.ts
+++ JS-TS-Fundamentals/TS in HTML/dvdLogoAnimation/dvdLogoAnimation.ts
@@ -4,128 +4,81 @@
 // Dodaj możliwość zmiany szerokości i wysokości zdjęcia, szybkości poruszania się przy pomocy inputa typu "range"
 // Licznik oraz ustawienia zdjęcia mają wyświetlać się w specjalnym modalu.
 
-const canvas = document.getElementById('mainCanvas')! as HTMLCanvasElement;
+const canvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
+const c = canvas.getContext('2d');
+const speed = document.getElementById('speed');
+const size = document.getElementById('size') as HTMLInputElement;
+const velocity = document.getElementById!('speed') as HTMLInputElement;
+const counter = document.getElementById('counter') as HTMLParagraphElement;
 
-let x;
-let y;
-let xspeed;
-let yspeed;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 function preparePlayground() {
-  //   canvas.width = document.body.clientWidth;
-  //   canvas.height = document.body.clientHeight;
-  const ctx = canvas.getContext('2d');
-  //   ctx.fillStyle = 'black';
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  //   const img = new Image();
-  //   const img = document.createElement('img');
-  //   img.src = 'dvd_logo.png';
-  //   console.log(img);
-
-  //   ctx.fillStyle = 'blue';
-  // ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  //   ctx.drawImage(image, x, y, width, height);
-  //   ctx?.drawImage(
-  //     img,
-  //     Math.random() * window.innerWidth,
-  //     Math.random() * window.innerHeight,
-  //     img.width,
-  //     img.height
-  //   );
-
-  //   img.onload = function () {
-  //     ctx.drawImage(img, 0, 0, img.width * 0.4, img.height * 0.4);
-  //   };
-
-  //   ctx.drawImage(img, 100, 100, 256, 256);
+  c!.fillStyle = 'black';
+  c!.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 class DvdLogo {
+  color;
+  counterWallBounce = 0;
   constructor(
     public x: number,
     public y: number,
     public width: number,
     public height: number,
-    public color: string
+    public velocity: { x: number; y: number }
   ) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.color = color;
+    this.color = this.changeColor();
+    this.velocity = velocity;
   }
 
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.fillStyle = this.color;
-    // ctx.beginPath();
-    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    // ctx.fillStyle = this.color;
-    // ctx.fill();
+    c!.fillStyle = this.color;
+    c!.fillRect(this.x, this.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+    if (this.x + this.width > canvas.width || this.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.bounceWall();
+    }
+    if (this.y + this.height > canvas.height || this.y <= 0) {
+      this.velocity.y = -this.velocity.y;
+      this.bounceWall();
+    }
+  }
+  changeColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  bounceWall() {
+    this.color = this.changeColor();
+    this.counterWallBounce++;
+    counter.innerText = `Had bumped into wall ${this.counterWallBounce} times`;
   }
 }
 
+function animate() {
+  requestAnimationFrame(animate);
+  preparePlayground();
+  dvdLogo.update();
+}
+
+function updateDVD() {
+  dvdLogo = new DvdLogo(20, 20, +size.value, +size.value, {
+    x: +velocity.value,
+    y: +velocity.value,
+  });
+}
+
+let dvdLogo: DvdLogo;
+updateDVD();
 preparePlayground();
-const dvdLogo = new DvdLogo(10, 10, 100, 100, 'blue');
-dvdLogo.draw();
-// let x;
-// let y;
-
-// let xspeed;
-// let yspeed;
-
-// let dvd;
-
-// let r, g, b;
-
-// function preload() {
-//   dvd = loadImage('dvd_logo.png');
-// }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   x = random(width);
-//   y = random(height);
-//   xspeed = 10;
-//   yspeed = 10;
-//   pickColor();
-// }
-
-// function pickColor() {
-//   r = random(100, 256);
-//   g = random(100, 256);
-//   b = random(100, 256);
-// }
-
-// function draw() {
-//   background(0);
-//   // rect(x, y, 80, 60);
-//   // Draw the DVD logo
-//   tint(r, g, b);
-//   image(dvd, x, y);
-
-//   x = x + xspeed;
-//   y = y + yspeed;
-
-//   if (x + dvd.width >= width) {
-//     xspeed = -xspeed;
-//     x = width - dvd.width;
-//     pickColor();
-//   } else if (x <= 0) {
-//     xspeed = -xspeed;
-//     x = 0;
-//     pickColor();
-//   }
-
-//   if (y + dvd.height >= height) {
-//     yspeed = -yspeed;
-//     y = height - dvd.height;
-//     pickColor();
-//   } else if (y <= 0) {
-//     yspeed = -yspeed;
-//     y = 0;
-//     pickColor();
-//   }
-// }
+animate();
